d2b264443b2f6383fef75e3cebde6f9684dd1771
JMay <jmay818@gmail.com>
2024-05-16 22:09:07 -0400

    Updated app

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
+const session = require('express-session');
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username) => { //returns boolean
+    //write code to check is the username is valid
     function User(user) {
         if (user.username === username) {
             return true;
@@ -19,8 +20,8 @@ const isValid = (username)=>{ //returns boolean
     return true;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password) => { //returns boolean
+    //write code to check if username and password match the one we have in records.
     function User(user) {
         return (user.username === username && user.password === password);
     }
@@ -33,7 +34,7 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
+    //Write your code here
     const username = req.body.username;
     const password = req.body.password;
     if (authenticatedUser(username, password)) {
@@ -51,8 +52,21 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    const review = req.query.review.split('+').join(' ');
+    const values = Object.values(books);
+    values[isbn - 1]["reviews"][username] = review;
+    return res.send("Review was added!");
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    const values = Object.values(books);
+    delete values[isbn - 1]["reviews"][username];
+    return res.send("Review deleted.");
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,70 +6,70 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  const username = req.body.username;
-  const password = req.body.password;
-  if (!username || !password) {
-    return res.send("Error. Unable to complete registration.")
-  }
-  if (username.trim().length < username.length || password.trim().length < password.length) {
-    return res.send("Username/password must not contain any spaces.")
-  }
-  if (!isValid(username)) {
-    return res.send("Please choose another username.")
-  } else {
-    users.push({"username": username, "password": password});
-    return res.send("Registration successful!")
-  }
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        return res.send("Error. Unable to complete registration.")
+    }
+    if (username.trim().length < username.length || password.trim().length < password.length) {
+        return res.send("Username/password must not contain any spaces.")
+    }
+    if (!isValid(username)) {
+        return res.send("Please choose another username.")
+    } else {
+        users.push({"username": username, "password": password});
+        return res.send("Registration successful!")
+    }
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.send(books);
+public_users.get('/', function (req, res) {
+    //Write your code here
+    return res.send(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
-  return res.send(books[isbn]);
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    //Write your code here
+    const isbn = req.params.isbn;
+    return res.send(books[isbn]);
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  const author = req.params.author.split('+').join(' ');
-  const arr = [];
-  const values = Object.values(books);
-  for (let i = 0; i < values.length; i++) {
-    if (values[i]["author"] == author ) {
-        arr.push(books[i + 1]);
+public_users.get('/author/:author', function (req, res) {
+    //Write your code here
+    const author = req.params.author.split('+').join(' ');
+    const arr = [];
+    const values = Object.values(books);
+    for (let i = 0; i < values.length; i++) {
+        if (values[i]["author"] == author ) {
+            arr.push(books[i + 1]);
+        }
     }
-  }
-  return res.send(arr);
+    return res.send(arr);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  const title = req.params.title.split('+').join(' ');
-  const arr = [];
-  const values = Object.values(books);
-  for (let i = 0; i < values.length; i++) {
-    if (values[i]["title"] == title ) {
-        arr.push(books[i + 1]);
+public_users.get('/title/:title', function (req, res) {
+    //Write your code here
+    const title = req.params.title.split('+').join(' ');
+    const arr = [];
+    const values = Object.values(books);
+    for (let i = 0; i < values.length; i++) {
+        if (values[i]["title"] == title ) {
+            arr.push(books[i + 1]);
+        }
     }
-  }
-  return res.send(arr);
+    return res.send(arr);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;
-  const values = Object.values(books);
-  return res.send(values[isbn - 1]["reviews"]);
+    //Write your code here
+    const isbn = req.params.isbn;
+    const values = Object.values(books);
+    return res.send(values[isbn - 1]["reviews"]);
 });
 
 module.exports.general = public_users;
